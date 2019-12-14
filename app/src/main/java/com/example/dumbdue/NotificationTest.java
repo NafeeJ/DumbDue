85ac10da3f212043656c1ec876b11bb550202c43
KevinTram <thekevintram@gmail.com>
2019-12-14 13:04:06 -0500

    Removed all traces of navigation drawer Created main page with a button When that button is pressed a notification is sent to the phone with a sound, title, and message

--- app/src/main/java/com/example/dumbdue/NotificationTest.java
+++ app/src/main/java/com/example/dumbdue/NotificationTest.java
@@ -7,23 +7,24 @@ import android.app.NotificationManager;
 import android.os.Build;
 
 public class NotificationTest extends Application {
-    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_1_ID = "channel1"; //string for channel ID
 
 
     @Override
-    public void onCreate() {
+    public void onCreate() {//creates notification channel on creation of app
         super.onCreate();
 
         createNotificationChannels();
     }
 
     private void createNotificationChannels() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID, "boop boop", NotificationManager.IMPORTANCE_HIGH);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //checks if andriod version is greater than or equal to Oreo
+            NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID, "I am telling you to get to work", NotificationManager.IMPORTANCE_HIGH); //creates instance of notification channel
 
-            channel1.setDescription("Bitch get yo ass to work");
+            channel1.setDescription("Bitch get yo ass to work"); //sets description of notification channel
 
-            NotificationManager manager = getSystemService(NotificationManager.class);
+            NotificationManager manager = getSystemService(NotificationManager.class); //creates a notification manager
+            manager.createNotificationChannel(channel1); //notification manager creates channel1 notification channel
 
         }
     }
