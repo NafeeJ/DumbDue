22d1bfaa7aaf577af17bab74310d0c0fa3d383a4
KevinTram <thekevintram@gmail.com>
2019-12-18 15:56:08 -0500

    Changed Minimum API level from 14 to 26

--- app/src/main/java/com/kiwicorp/dumbdue/MainActivity.kt
+++ app/src/main/java/com/kiwicorp/dumbdue/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var notificationManger: NotificationManagerCompat
@@ -22,12 +23,14 @@ class MainActivity : AppCompatActivity() {
         val title = "I am the title"
 
         val notification1 : Notification = NotificationCompat.Builder(this, NotificationTest.CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_android_black_24dp)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .build()
+            .setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentText(message)
+            .setContentTitle(title)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
 
         notificationManger.notify(1, notification1)
+
+
     }
 }
--- app/src/main/java/com/kiwicorp/dumbdue/NotificationTest.kt
+++ app/src/main/java/com/kiwicorp/dumbdue/NotificationTest.kt
@@ -10,7 +10,7 @@ import android.content.Context
 class NotificationTest : android.app.Application() {
 
     companion object {//equivalent as public static
-        const val CHANNEL_1_ID = "channel1"; //constant string for channel ID
+        const val CHANNEL_1_ID = "Channel1"//constant string for channel ID
     }
 
     override fun onCreate() {//creates notification channel on creation of app
@@ -20,11 +20,10 @@ class NotificationTest : android.app.Application() {
     }
 
     private fun createNotificationChannels() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//checks if andriod version is greater than or equal to Andriod Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//checks if android version is greater than or equal to Android Oreo
             val channel1 = NotificationChannel(CHANNEL_1_ID, "Notification Channel 1", NotificationManager.IMPORTANCE_HIGH )
 
             channel1.description = "Bitch get yo ass to work" //sets description of notification channel
-
 
             val manager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager //creates a notification manager
             manager.createNotificationChannel(channel1) //notification manager creates a channel1 notification channel
